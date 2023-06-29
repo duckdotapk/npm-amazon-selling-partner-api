211c2b24d1da22c899aaef358b064c652b79d6c8
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-06-29 09:42:47 -0400

    Adding AmazonSellingPartnerTokensAPIClient class.

---
+++ src/classes/AmazonSellingPartnerTokensAPIClient.ts
@@ -0,0 +1,47 @@
+//
+// Imports
+//
+
+import { AmazonSellingPartnerAPIClient } from "./AmazonSellingPartnerAPIClient.js";
+import { AmazonSellingPartnerAPIError } from "./AmazonSellingPartnerAPIError.js";
+
+import
+{
+	CreateRestrictedDataTokenRequest,
+	CreateRestrictedDataTokenResponse,
+	ErrorList,
+} from "../types/tokens-api/v2021-03-01.js";
+
+//
+// Class
+//
+
+/** A client for v2021-03-01 of the Tokens endpoints of the Amazon Selling Partner API. */
+export class AmazonSellingPartnerTokensAPIClient
+{
+	private readonly amazonSellingPartnerApiClient : AmazonSellingPartnerAPIClient;
+
+	constructor(amazonSellingPartnerApiClient : AmazonSellingPartnerAPIClient)
+	{
+		this.amazonSellingPartnerApiClient = amazonSellingPartnerApiClient;
+	}
+
+	async createRestrictedDataToken(body : CreateRestrictedDataTokenRequest) : Promise<CreateRestrictedDataTokenResponse>
+	{
+		const response = await this.amazonSellingPartnerApiClient.request(
+			{
+				method: "POST",
+				path: "/tokens/2021-03-01/restrictedDataToken",
+				body: JSON.stringify(body),
+			});
+
+		const responseData = await response.json() as ErrorList | CreateRestrictedDataTokenResponse;
+
+		if ("errors" in responseData)
+		{
+			throw new AmazonSellingPartnerAPIError(response, responseData.errors);
+		}
+
+		return responseData;
+	}
+}
---
+++ src/types/tokens-api/v2021-03-01.ts
@@ -0,0 +1,46 @@
+//
+// Amazon Selling Partner API > Tokens API > v2021-03-01
+//
+
+export interface CreateRestrictedDataTokenRequest
+{
+	targetApplication? : string;
+
+	restrictedResources : RestrictedResource[];
+}
+
+export interface CreateRestrictedDataTokenResponse
+{
+	restrictedDataToken : string;
+
+	expiresIn : number;
+}
+
+export interface Error
+{
+	code : string;
+
+	message : string;
+
+	details? : string;
+}
+
+export interface ErrorList
+{
+	// Note: This is labeled as "optional" in the docs but that makes NO sense.
+	errors : Error[];
+}
+
+export type Method = "GET" | "PUT" | "POST" | "DELETE";
+
+export interface RestrictedResource
+{
+	method : Method;
+
+	path : string;
+
+	dataElements : RestrictedResourceDataElement[];
+}
+
+// Note: Not a type in the docs but I think it's good to have it here.
+export type RestrictedResourceDataElement = "buyerInfo" | "shippingAddress" | "buyerTaxInformation";
